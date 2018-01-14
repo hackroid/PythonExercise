f8b412cfa11844b0383d49e61c412eafe4358ac9
Hackroid <ooozbh@gmail.com>
2018-01-14 20:40:59 +0800

    variable operation

---
+++ var.py
@@ -0,0 +1,13 @@
+str1 = '1e'
+str2 = '1+1+1'
+num1 = 123
+num2 = 1
+print(int(str1, 16))
+print(ord('の'))
+print(str(num1))  # Converts object x to a string representation
+print(repr(num1))  # Converts object x to an expression string
+print(eval(str2))  # Evaluates a string and returns an object
+# tuple(x) set(x) dict(x)
+print(chr(num2))  # Converts an integer to a character
+print(hex(num1))
+print(oct(num1))
